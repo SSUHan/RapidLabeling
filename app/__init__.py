f3b0ad839117c073ca6b894fa3a86f2eace69a80
이준수 <ljs93kr@gmail.com>
2017-05-21 15:06:38 +0900

    need to move datacenter to static

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 print("2", __name__)
 
 rc_app = Flask(__name__)
+# print(rc_app.root_path)
 rc_app.secret_key = 'mysecret'
 
 from app.routes import *
--- app/routes/index.py
+++ app/routes/index.py
@@ -1,7 +1,10 @@
 from app import rc_app
-from flask import render_template
+from flask import render_template, url_for
 import os
 @rc_app.route('/')
 def index_page():
-	image_path = 'app/datacenter/images/dog.jpg'
+	# image_path = 'app/datacenter/images/dog.jpg'
+	# image_path = os.path.join(rc_app.root_path, 'datacenter', 'images', 'dog.jpg')
+	image_path = url_for('static', filename='datacenter/images/dog.jpg')
+	print(image_path)
 	return render_template('index.html', image_path=image_path)
