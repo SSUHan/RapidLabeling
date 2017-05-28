06e5033fd38f4dff2cf804b2ab90e5de43a6f2c2
이준수 <ljs93kr@gmail.com>
2017-05-28 18:10:26 +0900

    video upload example success

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,5 +1,5 @@
 from app import rc_app, fm
-from flask import render_template, url_for, jsonify
+from flask import render_template, url_for, jsonify, request
 import os
 
 @rc_app.route('/')
@@ -10,22 +10,26 @@ def index_page():
 	
 	return render_template('index.html', image_path=image_path)
 
-@rc_app.route('/make_dir')
+@rc_app.route('/make_dir', methods=['POST'])
 def make_dir():
-	to_client = {}
-	new_hashid = fm.make_new_hashid()
-	to_client['new_hashid'] = new_hashid
-	datacenter_path = fm.make_datacenter_path(new_hashid, 'Junsu', 123)
-	to_client['datacenter_path'] = datacenter_path
-	# fm.make_new_datacenter()
+	if request.methods == 'POST'
+		to_client = {}
+		new_hashid = fm.make_new_hashid()
+		to_client['new_hashid'] = new_hashid
+		datacenter_path = fm.make_datacenter_path(new_hashid, 'Junsu', 123)
+
+		f = request.files['file']
+		print("f : ", f)
+		print("f.filename : ", f.filename)
+		# f.save(f.filename)
+
+		to_client['datacenter_path'] = datacenter_path
+
 	return jsonify(to_client)
 
 @rc_app.route('/video_upload', methods=['GET', "POST"])
 def video_upload():
 	to_client = {}
 	if request.method == 'POST':
-		f = request.files['file']
-		print("f : ", f)
-		f.save(f.filename)
-		return 'file uploaded successfully'
+		
 	return "no"
