4419ec5ce07b37e1064a27977ed7fcfae61f8383
SSUHan <ljs93kr@gmail.com>
2017-05-30 17:09:14 +0900

    Good work video manager

--- app/models/video_manager.py
+++ app/models/video_manager.py
@@ -1,7 +1,22 @@
 import cv2
 from os import sep
+import os
 
-def parse_video(video_path, images_path, frame_step):
+def make_frame_name(frameNum, space_size=6):
+	frame_num_str = str(frameNum)
+	zero_str = ''
+	for i in range(space_size-len(frame_num_str)):
+		zero_str += '0'
+
+	return zero_str+frame_num_str
+
+def parse_video(video_path, image_name, images_folder_path, frame_step):
+	"""
+		video_path : video file location
+		image_name : image prefix name
+		images_folder_path : dest image folder path
+		frame_step : step size for saving image from video
+	"""
 	cap = cv2.VideoCapture(video_path)
 	frameNum = 0
 	
@@ -14,7 +29,8 @@ def parse_video(video_path, images_path, frame_step):
 			continue
 		print("Save Image frameNum : ", frameNum)
 		resized_frame = cv2.resize(frame, (448, 448))
-		
+		cv2.imwrite(os.path.join(images_folder_path, image_name+"_{}.png".format(make_frame_name(frameNum))), resized_frame)
+		cv2.imshow('window', resized_frame)
 		if cv2.waitKey(0) & 0xFF == ord('q'):
 			break
 
@@ -22,4 +38,6 @@ def parse_video(video_path, images_path, frame_step):
 	cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-	parse_video('C:\\Users\\SoMa\\myworkspace\\object_detection_testset\\tracking.mp4', '.', 30)
+	parse_video('C:\\Users\\SoMa\\myworkspace\\object_detection_testset\\tracking.mp4', 
+				'tracking'
+				,'.', 30)
